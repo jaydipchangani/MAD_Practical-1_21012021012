e5a2d22659b0f3fd7280c66d9a8b9fbe45b288ad
Jaydip <pjaydip956@gmail.com>
2023-07-16 14:13:34 +0530

    16/7

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,23 @@
+fun main() {
+    println("Hello World !!")
+
+    var i:Int=12
+    var f:Double =12.44
+    var c:Char= 'J'
+    var d:Float=12.19F
+    var s:String="Jaydip"
+    var b:Boolean= true
+    var l:Long= 21012021012
+    var sh:Short= -12
+    var by:Byte= 119
+
+    println("Integer value: $i")
+    println("Double value: $d")
+    println("Character value: $c")
+    println("Float value: $f")
+    println("String value: $s")
+    println("Boolean value: $b")
+    println("Long value: $l")
+    println("Short value: $sh")
+    println("Byte value: $by")
+}
---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var i : Int =12
+    println("Integer Value: $i ")
+    println("Double Value (From Integer): ${i.toDouble()} ")
+
+    var s:String="12"
+    println("String Value: $s ")
+    println("Integer Value (From String): ${s.toInt()} ")
+    println("Double Value (From String): ${s.toDouble()} ")
+}
