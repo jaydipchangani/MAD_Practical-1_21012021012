fefac8b2066ea32185f599b81b4cc5460b3796c8
jaydip <pjaydip956@gmail.com>
2023-07-26 23:38:54 +0530

    26/7

--- src/main/kotlin/Practical-1.10.kt
+++ src/main/kotlin/Practical-1.10.kt
@@ -1,19 +1,49 @@
-
-fun main()
+class Car (
+    var type : String,
+    var model : Int,
+    var Oprice : Double,
+    var Cprice : Double,
+    var owner : String,
+    var milesdrive : Int)
 {
-    var s1 =student("Jaydip","21012021012")
-    println("Student name is ${s1.name}")
-    println("Student Enrollment no is ${s1.en}")
-}
-
-class student<string>(var name:string,er:string) {
-
-    var en: string = er
     init {
-        println("This is init class")
+        println("Object of class is created and Init is Called.")
+        println()
+    }
+    fun getOriginalCarPrice(): Double {
+        return Oprice
     }
 
+    fun getCurrentCarPrice(): Double {
+        return Cprice
+    }
 
+    fun displayCarInfo() {
+        val originalPrice = getOriginalCarPrice()
+        val currentPrice = getCurrentCarPrice()
+        println("-----------------------------------")
+        println("Car Information : $type, $model")
+        println("Car Owner : $owner")
+        println("Miles Drive : $milesdrive")
+        println("Original Car Price : $originalPrice")
+        println("Current Car Price : $currentPrice")
+        println("-----------------------------------")
+    }
 }
 
+fun main() {
+    var car1 = Car("BMW",2018, 100000.0,98950.0, "Aman",105 )
+    var car2 = Car("BMW",2019, 400000.0,399800.0, "Karan",20 )
+    car1.displayCarInfo()
+    car2.displayCarInfo()
+
+    var cars = ArrayList<Car>()
+    cars.add(Car("Toyota",2017, 1080000.0,1079000.0, "KJS",100 ))
+    cars.add(Car("Maruti",2020, 4000000.0,3998000.0, "NPP",200 ))
+    println("********** ArrayList of Car **********")
+    for (car in cars) {
+        car.displayCarInfo()
+        println()
+    }
+}
 
---
+++ src/main/kotlin/Practical-1.11.kt
@@ -0,0 +1,79 @@
+import javax.print.attribute.standard.MediaSize.Other
+
+class Matrix(var data: Array<IntArray>) {
+    val rows: Int = data.size
+    val columns: Int = data[0].size
+    operator fun plus(other: Matrix): Matrix {
+        val resultData  = Array(rows) {IntArray(columns)}
+        for (i in 0 until rows) {
+            for (j in 0 until columns) {
+                resultData[i][j] = data[i][j] + other.data[i][j]
+            }
+        }
+        return Matrix(resultData)
+    }
+
+    operator fun minus(other: Matrix): Matrix {
+        val resultData  = Array(rows) {IntArray(columns)}
+        for (i in 0 until rows) {
+            for (j in 0 until columns) {
+                resultData[i][j] = data[i][j] - other.data[i][j]
+            }
+        }
+        return Matrix(resultData)
+    }
+
+    operator fun times(other: Matrix): Matrix {
+        val resultData  = Array(rows) {IntArray(other.columns) {0} }
+        for (i in 0 until rows) {
+            for (j in 0 until other.columns) {
+                for (k in 0 until columns)
+                    resultData[i][j] += data[i][k] * other.data[k][j]
+            }
+        }
+        return Matrix(resultData)
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        for (i in 0 until rows) {
+            for (j in 0 until columns) {
+                sb.append("${data[i][j]}\t")
+            }
+            sb.append("\n")
+        }
+        return sb.toString()
+    }
+}
+fun main() {
+    val firstMatrix = Matrix(arrayOf(intArrayOf(2, 0, 4), intArrayOf(1,6,9)))
+    val secondMatrix = Matrix(arrayOf(intArrayOf(3, -6), intArrayOf(5, 1), intArrayOf(2,9)))
+    val secondMatrix1 = Matrix(arrayOf(intArrayOf(0, 7), intArrayOf(3,7), intArrayOf(6,7)))
+
+    println("********** Addition **********")
+    println("Matrix : 1 ")
+    println(secondMatrix1)
+    println("Matrix : 2 ")
+    println(secondMatrix)
+    val thirdMatrix = secondMatrix1 + secondMatrix
+    println("Addition : ")
+    println(thirdMatrix)
+
+    println("********** Subtraction **********")
+    println("Matrix : 1 ")
+    println(secondMatrix1)
+    println("Matrix : 2 ")
+    println(secondMatrix)
+    val subtractMatrix = secondMatrix1 - secondMatrix
+    println("Subtraction : ")
+    println(subtractMatrix)
+
+    println("********** Multiplication **********")
+    println("Matrix : 1 ")
+    println(firstMatrix)
+    println("Matrix : 2 ")
+    println(secondMatrix)
+    val multiplication = firstMatrix * secondMatrix
+    println("Multiplication : ")
+    println(multiplication)
+}
